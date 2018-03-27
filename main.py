1c21dd985a39aa49e0fcec7dd3c93bcc0facfdcc
Radhe Shyam Lodhi <radheshyamlodhi64@gmail.com>
2018-03-27 19:44:49 +0530

    Added main file

---
+++ main.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 26 23:59:50 2018
+
+@author: Radhe Shyam Lodhi
+"""
+
+import load_data as ld
+
+"""
+read dataset
+"""
+
+#tags, questions = ld.loadData()
+
+"""
+get 100 most frequent tags
+"""
+#freq_tags = ld.getFrequentTag(tags)
+
+
+#qId = ld.getQid(freq_tags,tags)
+
+
+"""
+create training set
+"""
+#training_set = ld.getTrainingset(qId,questions)
+
+
+"""
+create feature vector for body
+"""
+
+vocabulary,feature_vec = ld.getFeatureVector(training_set)
+    
+
+"""
+calculate tfidf
+"""
+
+tfidf_body = ld.getTfidf(feature_vec)
